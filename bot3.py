7b1a566c0d91062cb192c6cc79c68d07befde19e
Vishal Singh <vishal.aksingh@gmail.com>
2024-04-09 05:32:03 -0400

    commit before changing branch

--- bot3.py
+++ bot3.py
@@ -369,28 +369,34 @@ def plot_world_state(grid, bot):
     plt.subplot(133)
     plt.imshow(grid_img3)
     #plt.show()
-# g = Grid2()
-# b = bot3(g)
-# a = Alien(g._grid)
-# MAX_TURNS = 500
-# turns = 0
-# for _ in range(MAX_TURNS):
-#     print(f"Turn {_}")
-#     b.move()
-#     if g.grid[a.ind[1]][a.ind[0]].alien_belief == 0:
-#         print("Alien belief 0 at alien position!!!!")
-#     #plot_world_state(g, b)
-#     #plt.show()
-#     a.move()
-#     plot_world_state(g, b)
-#     plt.savefig(f"tmp{_}.png", dpi=200)
-#     plt.close()
-#     #plt.show()
-#     gif_coll.append(Image.open(f"tmp{_}.png"))
-#     turns += 1
-#     if g.crew_pos == b.pos:
-#         print("SUCCES: Crew member reached!")
-#         break
+
+g = Grid2()
+b = bot3(g)
+a = Alien(g._grid)
+MAX_TURNS = 500
+turns = 0
+for _ in range(MAX_TURNS):
+    print(f"Turn {_}")
+    b.move()
+
+    if g.crew_pos == None and g.crew_pos2 == None:
+        print("SUCCESS")
+        break
+
+    if g.grid[a.ind[1]][a.ind[0]].alien_belief == 0:
+        print("Alien belief 0 at alien position!!!!")
+    #plot_world_state(g, b)
+    #plt.show()
+    a.move()
+    plot_world_state(g, b)
+    plt.savefig(f"tmp{_}.png", dpi=200)
+    plt.close()
+    #plt.show()
+    gif_coll.append(Image.open(f"tmp{_}.png"))
+    turns += 1
+    if g.crew_pos == b.pos:
+        print("SUCCES: Crew member reached!")
+        break
 # print("Saving gif...")
 # #gif_coll[0].save('animated.gif', save_all=True, append_images=gif_coll, duratin=len(gif_coll)*0.2, loop=0)
 # os.system("ffmpeg -r 10 -i tmp%01d.png -vcodec mpeg4 -y -vb 400M movie.mp4")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,22 @@
-alien_pos
+g = Grid2()
+b = bot3(g)
+a = Alien(g._grid)
+MAX_TURNS = 500
+turns = 0
+for _ in range(MAX_TURNS):
+    print(f"Turn {_}")
+    b.move()
+    if g.grid[a.ind[1]][a.ind[0]].alien_belief == 0:
+        print("Alien belief 0 at alien position!!!!")
+    #plot_world_state(g, b)
+    #plt.show()
+    a.move()
+    plot_world_state(g, b)
+    plt.savefig(f"tmp{_}.png", dpi=200)
+    plt.close()
+    #plt.show()
+    gif_coll.append(Image.open(f"tmp{_}.png"))
+    turns += 1
+    if g.crew_pos == b.pos:
+        print("SUCCES: Crew member reached!")
+        break
